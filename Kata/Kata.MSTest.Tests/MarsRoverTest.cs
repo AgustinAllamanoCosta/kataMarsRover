ae14eec59e9205114189f9355f13c1c51e1fd764
AgustinAllamanoCosta <allamanocostaagustin@gmail.com>
2021-12-08 00:42:17 -0300

    Agrega test de mars rover para poder inicializar el objeto co

---
+++ Kata/Kata.MSTest.Tests/MarsRoverTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+
+namespace Kata.MSTest.Tests
+{
+    [TestClass]
+    public class MarsRoverTest
+    {
+        [TestMethod]
+        public void cuadnoCreoUnRoverSeLeAsignaUnaPosicionOrigenYUnaDireccionADondeMira() {
+
+            int[] poss = { 99, 99 };
+            string direccion = "N";
+
+            var rover = new MarsRover(poss,direccion);
+
+            Assert.AreEqual(poss, rover.posicion);
+            Assert.AreEqual( direccion, rover.direccion);
+        }
+    }
+}
---
+++ Kata/Kata/MarsRover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class MarsRover
+    {
+        private int[] poss;
+        private string dir;
+        public int[] posicion { set { poss = value; } get { return poss; } }
+        public string direccion { set { dir = value; } get { return dir; } }
+
+        public MarsRover(int[] posicion, string direcion) {
+            this.direccion = direcion;
+            this.posicion = posicion;
+        }
+
+    }
+}
